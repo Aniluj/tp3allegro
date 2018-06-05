7a4d42b8adedcd8b3eb439f537c9ff1f90019fd0
Aniluj <julianvolonterio@gmail.com>
2018-06-05 05:48:23 -0300

    Movimiento de enemigo 2 agregado, orden de codigo modificado e implementación de timer
    Se agregó un patrón de movimiento al enemigo 2 que fue agregado previamente.
    El código fue ordenado de una forma en la que se pueda entender con más claridad y por bloques, la razón por la cual son creadas algunas variables y su relación con lo que ocurrirá después. También se cambiaron algunos nombres de variables y se agregaron algunas que si bien no son utilizadas en este commit, posteriormente lo serán.
    Fue implementado un timer para que la fluidez con la que se ven los eventos en pantalla, sea acorde a los fps definidos al comienzo del código.

--- tp3sala-master/tp3Sala/tp3Sala/Source.cpp
+++ tp3sala-master/tp3Sala/tp3Sala/Source.cpp
@@ -19,22 +19,19 @@ int main(int argc, char **argv){
 
 
 	ALLEGRO_DISPLAY * display;
-	ALLEGRO_EVENT_QUEUE *queue;
 	ALLEGRO_BITMAP * playerBitmap = NULL;
 	ALLEGRO_BITMAP *enemieBitmap = NULL;
 	ALLEGRO_BITMAP *enemie2Bitmap = NULL;
 
 	al_init();
 
+	const float fps = 60.0;
 	const int displayWidth = 800;
 	const int displayHeight = 600;
 
 	display = al_create_display(displayWidth, displayHeight);
-	queue = al_create_event_queue();
 
 	al_install_keyboard();
-	al_register_event_source(queue, al_get_keyboard_event_source());
-	al_register_event_source(queue, al_get_display_event_source(display));
 	al_init_image_addon();
 
 	playerBitmap = al_load_bitmap("Blue_01.png");
@@ -46,12 +43,15 @@ int main(int argc, char **argv){
 	float widthPlayerBitmap = al_get_bitmap_width(playerBitmap);
 	float heightPlayerBitmap = al_get_bitmap_height(playerBitmap);
 
+	bool enemieGoingUp = false;
 	float widthEnemieBitmap = al_get_bitmap_width(enemieBitmap);
 	float heightEnemieBitmap = al_get_bitmap_height(enemieBitmap);
 
+	bool enemie2GoingRight = false;
 	float widthEnemie2Bitmap = al_get_bitmap_width(enemieBitmap);
 	float heightEnemie2Bitmap = al_get_bitmap_height(enemieBitmap);
 
+	int playerLife = 3;
 	float initialPlayerX = 0;
 	float initialPlayerY = (displayHeight / 2) - heightPlayerBitmap;
 	float playerX = initialPlayerX;
@@ -59,50 +59,90 @@ int main(int argc, char **argv){
 
 	float enemieX = 400;
 	float enemieY = 400;
+	float enemieMovementSpeed = 5.0f;
 
 	float enemie2X = 300;
 	float enemie2Y = 0;
+	float enemie2MovementSpeed = 5.0f;
 
-	float velocity = 4.5f;
+	float playerMovementSpeed = 4.5f;
 
 	cout << "width bitmap 1" << widthPlayerBitmap<<endl;
 	cout << "height bitmap 1" << heightPlayerBitmap << endl;
 
-	while (running) {
+	ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();;
+	ALLEGRO_KEYBOARD_STATE keyState;
+	ALLEGRO_TIMER *timer = al_create_timer(1.0 / fps);
+	al_register_event_source(queue, al_get_keyboard_event_source());
+	al_register_event_source(queue, al_get_timer_event_source(timer));
+
+	al_start_timer(timer);
+
+	while (running){
+
+		ALLEGRO_EVENT events;
+		al_wait_for_event(queue, &events);
+
 		al_clear_to_color(al_map_rgba_f(1, 1, 1, 1));
 		al_draw_bitmap(playerBitmap, playerX,playerY, 0);
 		al_draw_bitmap(enemieBitmap, enemieX, enemieY, 0);
 		al_draw_bitmap(enemie2Bitmap, enemie2X, enemie2Y, 0);
 		al_flip_display();
 
-		ALLEGRO_EVENT event;
-		al_wait_for_event(queue, &event);
-		if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+		if (events.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
 			running = false;
-	
-		ALLEGRO_KEYBOARD_STATE keyState;
-		al_get_keyboard_state(&keyState);
-		if (al_key_down(&keyState, ALLEGRO_KEY_RIGHT))
-		{
-			playerX += velocity;
-		}	
-		if (al_key_down(&keyState, ALLEGRO_KEY_LEFT))
-		{
-			playerX -= velocity;
-		}
-		if (al_key_down(&keyState, ALLEGRO_KEY_UP)) {
-			playerY -= velocity;
-		}
-		if (al_key_down(&keyState, ALLEGRO_KEY_DOWN)) {
-			playerY += velocity;
-		}
 
-		cout << "posx1 " << playerX << endl;
-		cout << "posx2 " << enemieX << endl;
+		if (events.type == ALLEGRO_EVENT_TIMER){
 
-		if (Collision(playerX, playerY, enemieX, enemieY, widthPlayerBitmap, heightPlayerBitmap, widthEnemieBitmap, heightEnemieBitmap) || Collision(playerX, playerY, enemie2X, enemie2Y, widthPlayerBitmap, heightPlayerBitmap, widthEnemie2Bitmap, heightEnemie2Bitmap)) {
-			cout << "contacto" << endl;
-			running = false;
+			al_get_keyboard_state(&keyState);
+			if (al_key_down(&keyState, ALLEGRO_KEY_RIGHT))
+			{
+				playerX += playerMovementSpeed;
+			}
+			if (al_key_down(&keyState, ALLEGRO_KEY_LEFT))
+			{
+				playerX -= playerMovementSpeed;
+			}
+			if (al_key_down(&keyState, ALLEGRO_KEY_UP)) {
+				playerY -= playerMovementSpeed;
+			}
+			if (al_key_down(&keyState, ALLEGRO_KEY_DOWN)) {
+				playerY += playerMovementSpeed;
+			}
+
+			cout << "posx1 " << playerX << endl;
+			cout << "posx2 " << enemieX << endl;
+
+			if (Collision(playerX, playerY, enemieX, enemieY, widthPlayerBitmap, heightPlayerBitmap, widthEnemieBitmap, heightEnemieBitmap) || Collision(playerX, playerY, enemie2X, enemie2Y, widthPlayerBitmap, heightPlayerBitmap, widthEnemie2Bitmap, heightEnemie2Bitmap)) {
+				cout << "contacto" << endl;
+				running = false;
+			}
+
+			if (!enemieGoingUp) {
+				enemieY += enemieMovementSpeed;
+			}
+			if (enemieGoingUp) {
+				enemieY -= enemieMovementSpeed;
+			}
+			if (enemieY + heightEnemieBitmap >= displayHeight) {
+				enemieGoingUp = true;
+			}
+			else if (enemieY <= 0) {
+				enemieGoingUp = false;
+			}
+
+			if (!enemie2GoingRight) {
+				enemie2X += enemie2MovementSpeed;
+			}
+			else {
+				enemie2X -= enemie2MovementSpeed;
+			}
+			if (enemie2X + widthEnemie2Bitmap >= displayWidth) {
+				enemie2GoingRight = true;
+			}
+			else if (enemie2X <= 0) {
+				enemie2GoingRight = false;
+			}
 		}
 	}
 
@@ -110,6 +150,8 @@ int main(int argc, char **argv){
 	al_uninstall_keyboard();
 	al_destroy_bitmap(playerBitmap);
 	al_destroy_bitmap(enemieBitmap);
+	al_destroy_bitmap(enemie2Bitmap);
+	al_destroy_timer(timer);
 
 	return 0;
 }
